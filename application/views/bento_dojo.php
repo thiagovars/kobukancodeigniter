b32c7dbae712a94235a290aeda1363765b90e526
Thiago Silveira <thiagovars@gmail.com>
2018-03-21 17:20:36 -0300

    Consertando caminho de imagens do dojo de bento

--- application/views/bento_dojo.php
+++ application/views/bento_dojo.php
@@ -78,7 +78,7 @@
 							<li class="col-md-3 isotope-item leadership">
 								<div class="team-item thumbnail">
 									<span class="thumb-info team">
-										<img class="img-responsive" alt="" src="<?php echo APPPATH; ?>img/team/eduardo.jpg">
+										<img class="img-responsive" alt="" src="/img/team/eduardo.jpg">
 										<span class="thumb-info-title">
 											<span class="thumb-info-inner">Eduardo</span>
 											<span class="thumb-info-type">Yondan (4º Dan)</span>
@@ -92,7 +92,7 @@
 							<li class="col-md-3 isotope-item leadership">
 								<div class="team-item thumbnail">
 									<span class="thumb-info team">
-										<img class="img-responsive" alt="" src="<?php echo APPPATH ?>img/team/ze.jpg">
+										<img class="img-responsive" alt="" src="/img/team/ze.jpg">
 										<span class="thumb-info-title">
 											<span class="thumb-info-inner">José Paulo</span>
 											<span class="thumb-info-type">1º kyu</span>
@@ -132,7 +132,7 @@
 								<ul class="lightbox" data-plugin-options='{"delegate": "a", "type": "image", "gallery": {"enabled": true}}'>
 									<li class="col-md-3 no-pin isotope-item">
 										<div class="portfolio-item img-thumbnail">
-											<a href="img/bento/inauguracao_bento.jpg" class="thumb-info">
+											<a href="/img/bento/inauguracao_bento.jpg" class="thumb-info">
 												<img src="/img/bento/inauguracao_bento.jpg" class="image-responsive" height="200" width="400">
 											</a>
 										</div>
