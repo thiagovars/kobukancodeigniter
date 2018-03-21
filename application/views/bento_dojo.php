92087dedd99aa6699dd69141ffb77573f7e44b04
Thiago Silveira <thiagovars@gmail.com>
2018-03-21 07:57:29 -0300

    Teste de caminho até a imagem
    Em bento dojo estou testando o caminho real até a imagem por dentro do framework

--- application/views/bento_dojo.php
+++ application/views/bento_dojo.php
@@ -78,7 +78,7 @@
 							<li class="col-md-3 isotope-item leadership">
 								<div class="team-item thumbnail">
 									<span class="thumb-info team">
-										<img class="img-responsive" alt="" src="img/team/eduardo.jpg">
+										<img class="img-responsive" alt="" src="<?php echo APPPATH ?>img/team/eduardo.jpg">
 										<span class="thumb-info-title">
 											<span class="thumb-info-inner">Eduardo</span>
 											<span class="thumb-info-type">Yondan (4º Dan)</span>
@@ -92,7 +92,7 @@
 							<li class="col-md-3 isotope-item leadership">
 								<div class="team-item thumbnail">
 									<span class="thumb-info team">
-										<img class="img-responsive" alt="" src="img/team/ze.jpg">
+										<img class="img-responsive" alt="" src="<?php echo APPPATH ?>img/team/ze.jpg">
 										<span class="thumb-info-title">
 											<span class="thumb-info-inner">José Paulo</span>
 											<span class="thumb-info-type">1º kyu</span>
