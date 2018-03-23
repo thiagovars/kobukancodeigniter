838f84c0dffc7a6ed409ef9518bdea0d8b784515
Thiago Silveira <thiagovars@gmail.com>
2018-03-23 16:29:03 -0300

    Conserto dos caminhos no menu

--- assets/menu.php
+++ assets/menu.php
@@ -1,7 +1,7 @@
         <div class="container">
           <h1 class="logo">
             <a href="/">
-              <img alt="Kobukan" width="190" height="150" data-sticky-width="82" data-sticky-height="70" src="<?php echo base_url('/img/logo.jpg'); ?>">
+              <img alt="Kobukan" width="190" height="150" data-sticky-width="82" data-sticky-height="70" src="<?php echo base_url('/img/logo.jpg') ?>">
             </a>
           </h1>
           <button class="btn btn-responsive-nav btn-inverse" data-toggle="collapse" data-target=".nav-main-collapse">
@@ -21,10 +21,10 @@
                     <i class="icon icon-angle-down"></i>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url('index.php/sobreaikido'); ?>"    >Sobre Aikido</a></li>
-                    <li><a href="<?php echo base_url('index.php/mestres#Osensei'); ?>"      >O'Sensei</a></li>
-                    <li><a href="<?php echo base_url('index.php/mestres#Kawaisensei'); ?>"  >Kawai Shihan</a></li>
-                    <li><a href="<?php echo base_url('index.php/mestres#Eduardosensei'); ?>">Eduardo Sensei</a></li>
+                    <li><a href="/index.php/sobreaikido"    >Sobre Aikido</a></li>
+                    <li><a href="/index.php/mestres#Osensei"      >O'Sensei</a></li>
+                    <li><a href="/index.php/mestres#Kawaisensei"  >Kawai Shihan</a></li>
+                    <li><a href="/index.php/mestres#Eduardosensei">Eduardo Sensei</a></li>
                   </ul>
                 </li>
                 <li class="dropdown">
@@ -33,32 +33,32 @@
                     <i class="icon icon-angle-down"></i>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url(); ?>Bentodojo"      >Bento Gonçalves</a></li>
-                    <li><a href="<?php echo base_url(); ?>Capivaridojo"   >Capivari de baixo</a></li>
-                    <li><a href="<?php echo base_url(); ?>Farroupilhadojo">Farroupilha</a></li>
-                    <li><a href="<?php echo base_url(); ?>Garopabadojo"   >Garopaba</a></li>
-                    <li><a href="<?php echo base_url(); ?>Gramadodojo"    >Gramado</a></li>
-                    <li><a href="<?php echo base_url(); ?>Icaradojo"      >Içara</a></li>
-                    <li><a href="<?php echo base_url(); ?>Imaruidojo"     >Imarui</a></li>
-                    <li><a href="<?php echo base_url(); ?>Imbitubadojo"   >Imbituba</a></li>
-                    <li><a href="<?php echo base_url(); ?>Ivotidojo"      >Ivoti</a></li>
-                    <li><a href="<?php echo base_url(); ?>Lagunadojo"     >Laguna</a></li>
-                    <li><a href="<?php echo base_url(); ?>Petropolisdojo" >Nova Petrópolis</a></li>
-                    <li><a href="<?php echo base_url(); ?>Portoalegredojo">Porto Alegre</a></li>
-                    <li><a href="<?php echo base_url(); ?>Tubaraodojo"    >Tubarão</a></li>
+                    <li><a href="/index.php/Bentodojo"      >Bento Gonçalves</a></li>
+                    <li><a href="/index.php/Capivaridojo"   >Capivari de baixo</a></li>
+                    <li><a href="/index.php/Farroupilhadojo">Farroupilha</a></li>
+                    <li><a href="/index.php/Garopabadojo"   >Garopaba</a></li>
+                    <li><a href="/index.php/Gramadodojo"    >Gramado</a></li>
+                    <li><a href="/index.php/Icaradojo"      >Içara</a></li>
+                    <li><a href="/index.php/Imaruidojo"     >Imarui</a></li>
+                    <li><a href="/index.php/Imbitubadojo"   >Imbituba</a></li>
+                    <li><a href="/index.php/Ivotidojo"      >Ivoti</a></li>
+                    <li><a href="/index.php/Lagunadojo"     >Laguna</a></li>
+                    <li><a href="/index.php/Petropolisdojo" >Nova Petrópolis</a></li>
+                    <li><a href="/index.php/Portoalegredojo">Porto Alegre</a></li>
+                    <li><a href="/index.php/Tubaraodojo"    >Tubarão</a></li>
                   </ul>
                 </li>
                 <li class="">
-                  <a href="<?php echo base_url(); ?>Iaido">Iaido</a>
+                  <a href="/index.php/Iaido">Iaido</a>
                 </li>
                 <li class="">
-                  <a href="<?php echo base_url(); ?>Jodo">Jodo</a>
+                  <a href="/index.php/Jodo">Jodo</a>
                 </li>
                 <li class="">
-                  <a href="<?php echo base_url(); ?>Naginata">Naginata</a>
+                  <a href="/index.php/Naginata">Naginata</a>
                 </li>
                 <li class="">
-                  <a href="<?php echo base_url(); ?>Fotos">Fotos</a>
+                  <a href="/index.php/Fotos">Fotos</a>
                 </li>
                 <li class="dropdown ">
                   <a href="#" class="dropdown-toggle">
@@ -66,8 +66,8 @@
                     <i class="icon icon-angle-down"></i>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url(); ?>Glossario">Informações sobre os treinos</a></li>
-                    <li><a href="<?php echo base_url(); ?>Tecnicas">Técnicas por graduação (5º ao 1º kyu)</a></li>
+                    <li><a href="/index.php/Glossario">Informações sobre os treinos</a></li>
+                    <li><a href="/index.php/Tecnicas">Técnicas por graduação (5º ao 1º kyu)</a></li>
                   </ul>
                 </li>
                 <li class="dropdown ">
@@ -76,19 +76,19 @@
                     <i class="icon icon-angle-down"></i>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url(); ?>Bonsai"         >Bonsai</a></li>
-                    <li><a href="<?php echo base_url(); ?>Chado"          >Chadô</a></li>
-                    <li><a href="<?php echo base_url(); ?>Fengshui"       >Fengshui</a></li>
-                    <li><a href="<?php echo base_url(); ?>Furoshiki"      >Furoshiki</a></li>
-                    <li><a href="<?php echo base_url(); ?>Kado"           >kadô</a></li>
-                    <li><a href="<?php echo base_url(); ?>Origami"        >Origami</a></li>
-                    <li><a href="<?php echo base_url(); ?>Shodo"          >Shodo</a></li>
-                    <li><a href="<?php echo base_url(); ?>Sumie"          >Sumie</a></li>
-                    <li><a href="<?php echo base_url(); ?>Culturaoriental">Fotos</a></li>
+                    <li><a href="/index.php/Bonsai"         >Bonsai</a></li>
+                    <li><a href="/index.php/Chado"          >Chadô</a></li>
+                    <li><a href="/index.php/Fengshui"       >Fengshui</a></li>
+                    <li><a href="/index.php/Furoshiki"      >Furoshiki</a></li>
+                    <li><a href="/index.php/Kado"           >kadô</a></li>
+                    <li><a href="/index.php/Origami"        >Origami</a></li>
+                    <li><a href="/index.php/Shodo"          >Shodo</a></li>
+                    <li><a href="/index.php/Sumie"          >Sumie</a></li>
+                    <li><a href="/index.php/Culturaoriental">Fotos</a></li>
                   </ul>
                 </li>
                 <li class="">
-                  <a href="<?php echo base_url(); ?>Contato">Contato</a>
+                  <a href="/index.php/Contato">Contato</a>
                 </li>
                 <!-- <li class="">
                   <a href="artigos.php">Artigos</a>
@@ -99,8 +99,8 @@
                     <i class="icon icon-angle-down"></i>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url(); ?>Acupuntura">Acupuntura</a></li>
-                    <li><a href="<?php echo base_url(); ?>Shiatsu">Shiatsu</a></li>
+                    <li><a href="/index.php/Acupuntura">Acupuntura</a></li>
+                    <li><a href="/index.php/Shiatsu">Shiatsu</a></li>
                   </ul>
                 </li>
                 
