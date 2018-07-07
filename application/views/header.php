f24099d9c867d554642e6955848dba71be79a01e
Pedro Silveira <pedro.silveira@ditech.com.br>
2018-07-07 01:09:23 -0300

    Conserto dos caminhos css orientando a partir do base_url

--- application/views/header.php
+++ application/views/header.php
@@ -18,58 +18,58 @@
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
 
     <!-- Libs CSS -->
-    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="/vendor/owl-carousel/owl.carousel.css" media="screen">
-    <link rel="stylesheet" href="/vendor/owl-carousel/owl.theme.css" media="screen">
-    <link rel="stylesheet" href="/vendor/magnific-popup/magnific-popup.css" media="screen">
-    <link rel="stylesheet" href="/vendor/isotope/jquery.isotope.css" media="screen">
-    <link rel="stylesheet" href="/vendor/mediaelement/mediaelementplayer.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/owl-carousel/owl.carousel.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/owl-carousel/owl.theme.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/magnific-popup/magnific-popup.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/isotope/jquery.isotope.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/mediaelement/mediaelementplayer.css" media="screen">
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" href="/css/theme.css">
-    <link rel="stylesheet" href="/css/theme-elements.css">
-    <link rel="stylesheet" href="/css/theme-blog.css">
-    <link rel="stylesheet" href="/css/theme-shop.css">
-    <link rel="stylesheet" href="/css/theme-animate.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme-elements.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme-blog.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme-shop.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme-animate.css">
 
     <!-- Current Page CSS -->
-    <link rel="stylesheet" href="/vendor/rs-plugin/css/settings.css" media="screen">
-    <link rel="stylesheet" href="/vendor/circle-flip-slideshow/css/component.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/rs-plugin/css/settings.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url();?>/vendor/circle-flip-slideshow/css/component.css" media="screen">
 
     <!-- Responsive CSS -->
-    <link rel="stylesheet" href="/css/theme-responsive.css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/theme-responsive.css" />
 
     <!-- Skin CSS -->
-    <link rel="stylesheet" href="/css/skins/default.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/skins/default.css">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/custom.css">
 
     <!-- Head Libs -->
-    <script src="/vendor/modernizr.js"></script>
-    <script src="/vendor/jquery.js"></script>
-    <script src="/vendor/jquery.appear.js"></script>
-    <script src="/vendor/jquery.easing.js"></script>
-    <script src="/vendor/jquery.cookie.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.js"></script>
-    <script src="/vendor/jquery.validate.js"></script>
-    <script src="/vendor/jquery.stellar.js"></script>
-    <script src="/vendor/jquery.knob.js"></script>
-    <script src="/vendor/jquery.gmap.js"></script>
-    <script src="/vendor/twitterjs/twitter.js"></script>
-    <script src="/vendor/isotope/jquery.isotope.js"></script>
-    <script src="/vendor/owl-carousel/owl.carousel.js"></script>
-    <script src="/vendor/jflickrfeed/jflickrfeed.js"></script>
-    <script src="/vendor/magnific-popup/magnific-popup.js"></script>
-    <script src="/vendor/mediaelement/mediaelement-and-player.js"></script>
+    <script src="<?php echo base_url();?>/vendor/modernizr.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.appear.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.easing.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.cookie.js"></script>
+    <script src="<?php echo base_url();?>/vendor/bootstrap/js/bootstrap.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.validate.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.stellar.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.knob.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jquery.gmap.js"></script>
+    <script src="<?php echo base_url();?>/vendor/twitterjs/twitter.js"></script>
+    <script src="<?php echo base_url();?>/vendor/isotope/jquery.isotope.js"></script>
+    <script src="<?php echo base_url();?>/vendor/owl-carousel/owl.carousel.js"></script>
+    <script src="<?php echo base_url();?>/vendor/jflickrfeed/jflickrfeed.js"></script>
+    <script src="<?php echo base_url();?>/vendor/magnific-popup/magnific-popup.js"></script>
+    <script src="<?php echo base_url();?>/vendor/mediaelement/mediaelement-and-player.js"></script>
 
     <!-- Theme Initializer -->
-    <script src="/js/theme.plugins.js"></script>
-    <script src="/js/theme.js"></script>
+    <script src="<?php echo base_url();?>/js/theme.plugins.js"></script>
+    <script src="<?php echo base_url();?>/js/theme.js"></script>
 
     <!-- Custom JS -->
-    <script src="/js/custom.js"></script>
+    <script src="<?php echo base_url();?>/js/custom.js"></script>
 
     <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
