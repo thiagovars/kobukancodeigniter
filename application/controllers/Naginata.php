06d8301adf2b88c8b56c7a0d9379cb1157559ca6
Pedro Silveira <pedro.silveira@ditech.com.br>
2018-09-02 20:30:18 -0300

    Altearção da graduação de eduardo e angélica

--- application/controllers/Naginata.php
+++ application/controllers/Naginata.php
@@ -8,6 +8,18 @@ class Naginata extends CI_Controller {
 		$data = array(
 			'title' => 'Naginata | Espada lança',
 			'fotos' => get_fotos('naginata/fotos'),
+			'responsaveis' => array(
+				'Angélica Philippe' => array(
+					'foto' => '/img/team/angelica.jpg',
+					'short_name' => 'Angélica',
+					'graduation' => '2º kyu'
+				),
+				'Eduardo Corrêa' => array(
+					'foto' => '/img/team/eduardoNaginata.jpg',
+					'short_name' => 'Eduardo',
+					'graduation' => 'Ikkyu (Faixa marrom)'
+				),
+			),
 		);
 
 		$this->load->view('header', $data);
--- application/views/naginata.php
+++ application/views/naginata.php
@@ -37,50 +37,31 @@
 								Aconteceu no dia 25 de Novembro de 2017, Seminário de Naginata com exame de graduação. Angelica e Eduardo foram aprovados para 3º kyu.
 							</p>
 							<p class="drop-caps">
-								Eduardo graduou 2º kyu no dia 25 de Fevereiro de 2018.
+								Eduardo graduou 2º kyu no dia 25 de Fevereiro de 2018. No dia 2 de setembro gradou-se Ikkyu (faixa marrom) e Angélica 2º kyu.
 							</p>
 							<hr class="tall">
 
 							<div class="row">
 								<div class="col-md-12" data-appear-animation="fadeInDown">
-
-									<!-- <div class="row">
-										<div class="col-md-3">
-											<h4><strong>Responsável Técnico</strong></h4>
-										</div>
-										<div class="col-md-3">
-											<h4><strong>Instrutor</strong></h4>
-										</div>
-									</div> -->
-								<div class="row">
-											<ul class="team-list sort-destination" data-sort-id="team">
-												
+									<div class="row">
+										<ul class="team-list sort-destination" data-sort-id="team">
+											<?php foreach ($responsaveis as $responsavel => $dados) : ?>
 												<li class="col-md-3 isotope-item leadership">
 													<div class="team-item thumbnail">
 														<span class="thumb-info team">
-															<img class="img-responsive" alt="" src="<?php echo base_url('/img/team/angelica.jpg'); ?>">
+															<img class="img-responsive" alt="" src="<?php echo base_url($dados['foto']); ?>">
 															<span class="thumb-info-title">
-																<span class="thumb-info-inner">Angélica Philippe</span>
-																<span class="thumb-info-type">3º kyu</span>
+																<span class="thumb-info-inner"><?php echo $dados['short_name']; ?></span>
+																<span class="thumb-info-type"><?php echo $dados['graduation']; ?></span>
 															</span>
 														</span>
-													</div>
-												</li>							
-												
-												<li class="col-md-3 isotope-item leadership">
-													<div class="team-item thumbnail">
-														<span class="thumb-info team">
-															<img class="img-responsive" alt="" src="<?php echo base_url('/img/team/eduardoNaginata.jpg'); ?>">
-															<span class="thumb-info-title">
-																<span class="thumb-info-inner">Eduardo Corrêa</span>
-																<span class="thumb-info-type">2º kyu</span>
-															</span>
+														<span class="thumb-info-caption">
+															<p><?php echo $responsavel; ?></p>
 														</span>
 													</div>
-												</li>							
-
-											</ul>
-
+												</li>
+											<?php endforeach; ?>
+										</ul>
 									</div>
 								</div>
 							</div>
