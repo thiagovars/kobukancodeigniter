4d273cd1f3e22a0e483d072bce5a1346041344e3
João Paulo <joao@esteticafacil.com>
2020-05-24 17:46:44 -0300

    esconde tipos de erro de desenvolvimento no ambiente de produção

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT)
+switch (constant('ENVIRONMENT'))
 {
 	case 'development':
 		error_reporting(-1);
@@ -73,7 +73,7 @@ switch (ENVIRONMENT)
 	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
-		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT ^ E_USER_NOTICE ^ E_USER_DEPRECATED);
 	break;
 
 	default:
