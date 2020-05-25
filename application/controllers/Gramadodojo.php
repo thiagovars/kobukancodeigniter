5eacbe7e576d06e2e6cf6318a061f246f3c7ffdf
João Paulo <joao@esteticafacil.com>
2020-05-24 22:05:31 -0300

    dojo gramado: horários

--- application/controllers/Gramadodojo.php
+++ application/controllers/Gramadodojo.php
@@ -3,26 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Gramadodojo extends CI_Controller {
 
-	public function index() 
+	public function index()
 	{
 		$this->load->helper('url');
 		$data = array(
 			'endereco' => 'Av. Borges de Medeiros, 4770 - Floresta, Gramado - RS',
 			'title' => 'Dojo Gramado ',
-			'title_treino_alternativo' => '<h2 class="shorter">Horários para treino <h3 class="shorter"><strong>Iaido, Jodo e Naginata</strong></h3></h2>',
+			'title_treino_alternativo' => '<h2 class="shorter">Horários para treino <h3 class="shorter"><strong>Iaido</strong></h3></h2>',
 			'title_treino_aikido' => '<h2 class="shorter">Horários para treino <h3 class="shorter"><strong>Aikido</strong></h3></h2>',
 			'config_horarios_md' => 'col-md-4',
-			'fotos' => get_fotos('gramado/fotos'),			
+			'fotos' => get_fotos('gramado/fotos'),
 			'contato' => array(
 				'Jordan T. Nicloti' => '(54) 99604 1112',
 			),
 			'horarios_alternativos' => array(
-				'Segundas' => array('hora' => '20h00 - 21h00', 'instrutor' => 'Jordan T. Nicloti'),
-				'Quartas'  => array('hora' => '20h00 - 21h00', 'instrutor' => 'Jordan T. Nicloti'),
+				'Segundas' => array('hora' => '21h00 - 22h00', 'instrutor' => 'Jordan T. Nicloti'),
 			),
 			'horarios_treino' => array(
 				'Segundas' => array('hora' => '20h00 - 21h00', 'instrutor' => 'Jordan T. Nicloti'),
-				'Quartas'  => array('hora' => '20h00 - 21h00', 'instrutor' => 'Jordan T. Nicloti'),
+				'Quartas'  => array('hora' => '20h00 - 22h00', 'instrutor' => 'Jordan T. Nicloti'),
 			),
 			'responsaveis' => array(
 				'Jordan Teixeira Nicloti' => array(
