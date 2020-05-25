964b88174d7a8d52ea0778e459437d2504bf2884
João Paulo <joao@esteticafacil.com>
2020-05-24 22:01:32 -0300

    dojo bento gonçalves: horários, endereço e responsável

--- application/controllers/Bentodojo.php
+++ application/controllers/Bentodojo.php
@@ -6,9 +6,9 @@ class Bentodojo extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url');
-		
+
 		$data = array(
-			'endereco' => 'R. Pernambuco, 281, centro - Bento Gonçalves - RS',
+			'endereco' => 'Black Box CT - R. Tocantins 265, São Bento - Bento Gonçalves - RS',
 			'title' => 'Dojo Bento Gonçalves',
 			'config_horarios_md' => 'col-md-8',
 			'title_treino_aikido' => '<h2 class="shorter">Horários para treino <strong>Aikido</strong></h2>',
@@ -17,14 +17,14 @@ class Bentodojo extends CI_Controller {
 				'José Aquini' => '(54) 9962 5249',
 			),
 			'horarios_treino' => array(
-				'Terças' => array('hora' => '20h00 - 21h00', 'instrutor' => 'José Aquini'),
-				'Quintas' => array('hora' => '20h00 - 21h00', 'instrutor' => 'José Aquini'),
+				'Segunda' => array('hora' => '20h00 - 21h00', 'instrutor' => 'José Aquini'),
+				'Quarta' => array('hora' => '20h00 - 21h00', 'instrutor' => 'José Aquini'),
 			),
 			'responsaveis' => array(
 				'José Paulo Aquini Camargo' => array(
 					'foto' => '/img/team/ze.jpg',
 					'short_name' => 'José Aquini',
-					'graduation' => 'Shodan (1º Dan)'
+					'graduation' => 'Nidan (2º Dan)'
 				),
 			),
 		);
