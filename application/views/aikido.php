0bb3c468e542a84eeef3c2e4339178a61c3671c8
João Paulo <joao@esteticafacil.com>
2020-08-13 18:17:31 -0300

    atualização página aikido

--- application/views/aikido.php
+++ application/views/aikido.php
@@ -46,25 +46,28 @@
 							<h3><strong>Sobre:</strong> Aikido</h3>
 							<p class="tall">
 								Caminho marcial japonês criado pelo grão-mestre Morihei Ueshiba (1883/1969).
-								Caracteriza-se por ser um Budô, ou seja, uma arte marcial dotada de filosofia 
-								verdadeira e caminho espiritual: o Aikido não possui competições pois o objetivo não é 
-								lutar contra o parceiro mas permitir o desenvolvimento espiritual por meio do exercício 
-								das técnicas. 
+								Caracteriza-se por ser um Budô, ou seja, uma arte marcial dotada de filosofia
+								verdadeira e caminho espiritual: o Aikido não possui competições pois o objetivo não é
+								lutar contra o parceiro mas permitir o desenvolvimento espiritual por meio do exercício
+								das técnicas.
 							</p>
 							<p class="tall">
-								Essa Arte marcial é baseada em esquivas, arremessos, torções nas articulações e 
-								imobilizações dos agressores. É derivado do Daitoryu Aikijiujitsu, arte marcial samurai 
-								praticada pelo clã Aizu. Durante séculos ficou restrita a apenas uma família (Takeda) 
-								e apenas em 1900 começou a ser ensinada aos militares, a nobreza ou graduados de 
+								Essa Arte marcial é baseada em esquivas, arremessos, torções nas articulações e
+								imobilizações dos agressores. É derivado do Daitoryu Aikijiujitsu, arte marcial samurai
+								praticada pelo clã Aizu. Durante séculos ficou restrita a apenas uma família (Takeda)
+								e apenas em 1900 começou a ser ensinada aos militares, a nobreza ou graduados de
 								outros budôs.
 							</p>
 							<p class="tall">
-								É conhecido como a “arte marcial da paz” pois não possui técnicas de ataque, mas 
-								apenas de defesa pessoal. Utiliza a espada e o bastão, armas tradicionais dos 
-								samurais, como um complemento e aperfeiçoamento das demais técnicas. Pode ser 
-								praticado por ambos os sexos, dos 6 aos 80 anos, pois utiliza a força do adversário em 
+								É conhecido como a “arte marcial da paz” pois não possui técnicas de ataque, mas
+								apenas de defesa pessoal. Utiliza a espada e o bastão, armas tradicionais dos
+								samurais, como um complemento e aperfeiçoamento das demais técnicas. Pode ser
+								praticado por ambos os sexos, dos 6 aos 80 anos, pois utiliza a força do adversário em
 								detrimento da força bruta.</p>
-						</div>						
+						</div>
+
+                        <!-- <iframe src="https://player.vimeo.com/video/443031872" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe> -->
+
 					</div>
 
 					<hr class="tall">
@@ -77,51 +80,51 @@
 <BR><BR>
 					<div class="row">
 						<div class="col-md-12">
-							<div class="row">								
+							<div class="row">
 								<div class="col-md-12" data-appear-animation="fadeInUp">
-									<p class="tall">O Aikido é um processo educacional para o desenvolvimento do corpo, da mente e do 
-									espírito. Na saúde corporal como em toda atividade física, a arte traz melhorias para o 
-									sistema circulatório pois a compressão das artérias e veias ajudam o bombeamento do 
+									<p class="tall">O Aikido é um processo educacional para o desenvolvimento do corpo, da mente e do
+									espírito. Na saúde corporal como em toda atividade física, a arte traz melhorias para o
+									sistema circulatório pois a compressão das artérias e veias ajudam o bombeamento do
 									sangue ao coração.</p>
 
-									<p class="tall">Os exercícios cheios de vigor, ajudam a manter um bom tônus muscular em pernas 
-									e braços. Faz bem ao sangue pois a melhora na circulação elimina radicais livres, 
-									toxinas e impurezas. Há uma melhora na postura do praticante em virtude do 
-									fortalecimento dos músculos esqueléticos, o que previne futuros problemas na coluna 
+									<p class="tall">Os exercícios cheios de vigor, ajudam a manter um bom tônus muscular em pernas
+									e braços. Faz bem ao sangue pois a melhora na circulação elimina radicais livres,
+									toxinas e impurezas. Há uma melhora na postura do praticante em virtude do
+									fortalecimento dos músculos esqueléticos, o que previne futuros problemas na coluna
 									vertebral.</p>
 
 									<img class="thumbnail" src="<?php echo base_url('/img/osensei2.jpg'); ?>" style="margin: 0 auto; margin-bottom: 20px;">
 
-									<p class="tall">Para a <strong>terceira idade</strong> as articulações devem ser trabalhadas, do contrário perdem a 
-									mobilidade, ficam atrofiadas e curvas. O “desempenho de elite” dos praticantes mais 
-									antigos promove um retardo na magnitude do declínio muscular e das articulações. 
-									Além disso, ativa positivamente na prevenção de quedas naturais da velhice. O Aikido 
-									reduz o decréscimo da estabilidade da terceira idade, pois a força muscular adequada, 
-									particularmente nas extremidades inferiores, restaura a estabilização que pode ser 
+									<p class="tall">Para a <strong>terceira idade</strong> as articulações devem ser trabalhadas, do contrário perdem a
+									mobilidade, ficam atrofiadas e curvas. O “desempenho de elite” dos praticantes mais
+									antigos promove um retardo na magnitude do declínio muscular e das articulações.
+									Além disso, ativa positivamente na prevenção de quedas naturais da velhice. O Aikido
+									reduz o decréscimo da estabilidade da terceira idade, pois a força muscular adequada,
+									particularmente nas extremidades inferiores, restaura a estabilização que pode ser
 									perdida. Há também uma melhora no tempo de reação aos acidentes.</p>
 
 									<img class="thumbnail" src="<?php echo base_url('/img/osensei4.jpg'); ?>" width="350" style="margin: 0 auto; margin-bottom: 20px;">
-									
+
 									<p class="tall">Para a <strong>crianças</strong>, além da parte relacionada à saúde com o incentivo da prática de
-									atividades físicas desde cedo, é na parte psicológica que o Aikido traz inúmeros 
-									benefícios. Por não possuir competições, o Aikido incentiva a cooperação e a 
-									sociabilização dos pequenos. Uns torcem pelos outros passarem nos exames de faixa 
-									e se ajudam mutuamente. De forma contrária aos esportes competitivos, no Aikido 
-									todos são vencedores e se sentem estimulados com os progressos, estimulando o 
-									desenvolvimento da auto-estima e da auto-confiança. Além disso, uma criança pouco 
-									estimulada transforma-se em um adulto com dificuldades de coordenação motora: o 
+									atividades físicas desde cedo, é na parte psicológica que o Aikido traz inúmeros
+									benefícios. Por não possuir competições, o Aikido incentiva a cooperação e a
+									sociabilização dos pequenos. Uns torcem pelos outros passarem nos exames de faixa
+									e se ajudam mutuamente. De forma contrária aos esportes competitivos, no Aikido
+									todos são vencedores e se sentem estimulados com os progressos, estimulando o
+									desenvolvimento da auto-estima e da auto-confiança. Além disso, uma criança pouco
+									estimulada transforma-se em um adulto com dificuldades de coordenação motora: o
 									Aikido potencializa as habilidades motoras que se desenvolvem a partir dos 5 anos.</p>
 
 									<p class="tall">Já para as <strong>mulheres</strong>, o fato de o Aikido não utilizar a força física a coloca em
-									igualdade de condições que os homens. Com a violência atingindo níveis alarmantes, 
-									o Aikido é uma excelente arte de defesa pessoal, preparando as mulheres para 
-									a “realidade das ruas”. As quedas realizadas nas aulas ajudam as mulheres a 
-									superarem eventuais medos ou traumas, visto que na infância as meninas são menos 
-									estimuladas que os meninos para as atividades físicas que envolvem quedas e 
-									rolamentos. O Aikido além de aumentar a disposição física feminina, ainda contribui 
-									psicologicamente para a mulher, pois ajuda a suportar a rotina desgastante do 
-									cotidiano frente aos inúmeros papéis que assume (mulher, mãe, esposa, trabalhadora, 
-									estudantes,...) Ao interessar-se pelo Budô a mulher encontra o sentido da harmonia no 
+									igualdade de condições que os homens. Com a violência atingindo níveis alarmantes,
+									o Aikido é uma excelente arte de defesa pessoal, preparando as mulheres para
+									a “realidade das ruas”. As quedas realizadas nas aulas ajudam as mulheres a
+									superarem eventuais medos ou traumas, visto que na infância as meninas são menos
+									estimuladas que os meninos para as atividades físicas que envolvem quedas e
+									rolamentos. O Aikido além de aumentar a disposição física feminina, ainda contribui
+									psicologicamente para a mulher, pois ajuda a suportar a rotina desgastante do
+									cotidiano frente aos inúmeros papéis que assume (mulher, mãe, esposa, trabalhadora,
+									estudantes,...) Ao interessar-se pelo Budô a mulher encontra o sentido da harmonia no
 									convívio familiar.</p>
 
 									<p class="tall">O aikido também propicia inúmeros benefícios à Saúde Mental. Desde criança o ser humano disputa várias competições durante a vida. Competimos por brinquedos, nos esportes, no vestibular, por empregos e para ganhar dinheiro. A sociedade de consumo gera ansiedade, inseguranças, estresses e até neuroses, que muitas vezes resultam em comportamentos mentais rígidos e solitários. Quando as pressões externas se tornam excessivas podem gerar frustrações e culminar com distorções da personalidade e de valores, muitas vezes arruinando os relacionamentos humanos. Assim o Aikido, por sua filosofia e métodos, é uma arte muito útil para as pessoas que estão na liderança com o trabalho conjunto. São fortalecidos sentimentos de estima e respeito aos colegas, e principalmente aos mais graduados que ajudam e serem de modelo a quem está ingressando no mundo do Budô. Os alunos disciplinam-se e tomam consciência da necessidade de não se machucar e nem machucar os outros. Os impulsos agressivos que levam as pessoas a procurar uma luta são controlados e direcionados a uma atividade construtiva. Os exames de faixa treinam a capacidade psicológica do aluno para lidar melhor com situações em que esteja sendo avaliado ou pressionado. O aumento do auto controle torna as pessoas mais flexíveis à idéias contrárias, transformando as críticas em ações positivas.</p>
